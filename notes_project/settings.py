908fb64a0b0010b24191f5a03fd85f1cc251ad31
Diego G <diego.gomes87@gmail.com>
2020-02-02 10:27:10 -0300

    add rest and core

--- notes_project/settings.py
+++ notes_project/settings.py
@@ -38,10 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
+    'rest_framework',
     'notes',
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
